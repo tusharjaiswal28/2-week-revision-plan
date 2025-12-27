6b194b0143a44a2cc2542fadb069e8221a615cf7
Tushar <tusharjaiswal8090@gmail.com>
2025-12-27 23:20:29 +0530

    js ques2

--- JS/Day2/Ques2/solution.js
+++ JS/Day2/Ques2/solution.js
@@ -16,5 +16,29 @@ const orders = [
   }
 ];
 
-let res={};
+let res=[];
 
+for(let i=0; i<orders.length; i++)
+{
+  let orId=orders[i].orderId;
+  let custName=orders[i].customer.name;
+  let city=orders[i].customer.location.city;
+
+  let totalPrice=0
+
+  for(let j=0; j<orders[i].items.length; j++)
+  {
+    totalPrice +=orders[i].items[j].price*orders[i].items[j].quantity;
+  }
+
+  let obj={
+    orderId:orId,
+    customerName:custName,
+    city:city,
+    totalAmount:totalPrice
+  };
+
+  res.push(obj)
+}
+
+console.log(res);
